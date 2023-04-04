1879709a1ca64bcf0c63f3bce9c4b20d53b85218
Juan Lopez <juelopezal@unal.edu.co>
2023-04-03 22:27:17 -0500

    Laboratiorio 5

--- pregunta.py
+++ pregunta.py
@@ -15,8 +15,7 @@ def clean_data():
     
     #
     # Inserte su código aquí
-    #
-    
+    #        
          
     df.rename(columns={"Unnamed: 0":"index"}, inplace=True)    
     df.set_index("index",inplace=True)
